7b51d7f8a67a4c8bf6dfa45de7006774e33dc690
Sadanan <micksudev@gmail.com>
2025-04-05 11:59:49 +0700

    [PHP] Fixed: CORS Policy.

--- php/items/diamond/subtract.php
+++ php/items/diamond/subtract.php
@@ -1,6 +1,8 @@
 <?php
 
 header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 require_once __DIR__ . '/../../db.php';
 
